ce43ab3271c95d59ed425e79fe3cea820870c15c
dDavilaEid <david.davila@electronicid.eu>
2022-01-24 23:44:32 +0100

    error test validate every error has message

--- src/importer/validators/errors.spec.ts
+++ src/importer/validators/errors.spec.ts
@@ -3,6 +3,6 @@ import { Errors } from './errors';
 describe('Errors', () => {
   it('should have value for every error key', async () => {
     const errors = Errors
-    expect(Object.keys(errors).length).toEqual(Object.values(errors).length)
+    expect(Object.keys(errors).length).toEqual(Object.values(errors).filter((msg: string) => msg).length)
   });
 });
